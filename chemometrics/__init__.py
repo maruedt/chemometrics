7b92833831756acd4d678cd1bc1a5e7b8b70bf53
Matthias Rüdt <mruedt@icloud.com>
2020-05-07 21:47:44 +0200

    Structure as packages.

---
+++ chemometrics/__init__.py
@@ -0,0 +1,16 @@
+"""
+Chemometrics: A chemometrics package for Python
+===============================================
+
+Documentation is available in the docstrings.
+
+Contents
+--------
+Chemometrics provides functions for preprocessing,
+factorizing, plotting, analyzing and fitting
+chemical information.
+"""
+
+
+from .preprocessing import *
+from .plotting import *

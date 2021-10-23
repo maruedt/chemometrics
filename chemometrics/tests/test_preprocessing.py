72ed28d63d6681eec79ed49e1af893c7c31423bd
Matthias Rüdt <mruedt@icloud.com>
2021-10-23 16:01:54 +0200

    Move base tests to chemometrics/tests/

--- chemometrics/tests/test_preprocessing.py
+++ chemometrics/tests/test_preprocessing.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with chemometrics.  If not, see <https://www.gnu.org/licenses/>.
 
-from .context import chemometrics as cm  # accounts for relativ path
+import chemometrics as cm  # accounts for relativ path
 from chemometrics import preprocessing as pp
 import numpy as np
 import unittest

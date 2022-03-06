dd56f75fc39b172df70041095c8f4ec51147f1a8
Matthias Rüdt <mruedt@icloud.com>
2022-03-06 13:32:45 +0100

    Implement function for mechanistic Voigt-based spectra generation
    - Add pseudo_voigt_spectra which generates mechanistic approximations of 
    spectra based on a given parameter and wavenumber range
    - Add tests to ensure correct behaviour

--- chemometrics/tests/test_utils.py
+++ chemometrics/tests/test_utils.py
@@ -18,6 +18,9 @@
 import chemometrics as cm  # accounts for relativ path
 import numpy as np
 import unittest
+from numpy.testing import assert_allclose
+from scipy.interpolate import interp1d
+from scipy.integrate import trapezoid
 
 
 class TestGenerate_spectra(unittest.TestCase):
@@ -44,3 +47,53 @@ class TestGenerate_spectra(unittest.TestCase):
         spectra = cm.generate_spectra(n_wl, n_bands, bandwidth)
         isZero = np.all(np.isclose(np.zeros(n_wl), spectra))
         self.assertTrue(isZero)
+
+class TestPsaudoVoigt(unittest.TestCase):
+    r"""
+    Test Pseudo Voigt profiles
+    """
+
+    def test_shape(self):
+        """
+        Test if _pseudo_voigt returns correct shape.
+        """
+
+        wl = np.arange(1000)[:, None]
+        parameters = np.array([
+            [200, 0.5, 50, 500],
+            [100, 0.8, 20, 200]
+        ]).T
+
+        spec = cm.pseudo_voigt_spectra(wl, parameters)
+
+        self.assertTrue(spec.shape == wl.shape)
+
+    def test_statistics_pdf(self):
+        """
+        Test area, mode and full width at half max of a single Voigt peak
+        in lorentzian limit
+        """
+        wl = np.arange(1000)[:, None]
+        par_list = [
+                np.array([[1, 0, 50, 500],]).T,
+                np.array([[1, 0.3, 50, 500],]).T,
+                np.array([[1, 1, 40, 280],]).T
+        ]
+
+
+        for parameters in par_list:
+            spec = cm.pseudo_voigt_spectra(wl, parameters)
+
+            # calculate mode (wl_max)
+            wl_max = wl[np.argmax(spec)][0]
+            assert_allclose(parameters[3], wl_max)
+            signal_max = np.max(spec)
+            assert_allclose(parameters[0], signal_max)
+
+            # calculate fwhm by inverting half (>wl_max) of the
+            # propability  density function and analyzing width
+
+            invPDF = interp1d(spec[wl_max:,0], wl[wl_max:,0])
+            fwhm = 2*(invPDF(signal_max/2)-wl_max)
+
+            assert_allclose(fwhm, 2*parameters[2])
--- chemometrics/utils.py
+++ chemometrics/utils.py
@@ -129,3 +129,67 @@ def _gaussian_fun(x, mu, sigma):
     Generates Gaussian profile
     """
     return np.exp(-((x - mu) / sigma) ** 2 / 2)
+
+
+def pseudo_voigt_spectra(x, parameter):
+    r"""
+    Generates vector based on pseudo-Voigt profiles
+
+    The function takes a wavenumber vector (x) and a set of parameters with the
+    shape 4xn. It generates a mechanistic approximation a spectrum based on
+    pseudo Voigt peaks.
+
+
+    Parameters
+    ----------
+    x : (n_wl, 1)
+        evaluated wavelengths
+    parameters : (4, n_peaks)
+        parameter definition for voigt profiles. parameters[0, :] define the
+        peak heights. parameters[1, :] defines percentage of Lorentzian to
+        Gauss profile. parameters[2, :] defines the peak location.
+        parameters[3, :] defines the half width at half height of the peak.
+
+    Returns
+    -------
+    spectra : (n_wl, 1)
+        spectra with n_peaks evaluated voigt profiles
+
+    Notes
+    -----
+    The pseudo-Voigt peaks are implemented similarly as described in [3]_.
+    However, the parameters are chosen such that they do only affect one
+    statistical property of the profile: The definition of the peak width is
+    selected such that the Full Width at Half Maximum (FWHM) does not change
+    when changing the relative contributions of the Gaussian/Lorentzian
+    profile. Similarly, the peak height is not affected by the other parameters
+    (peaks normalized by height not by area!). This approach to the peak height
+    was chosen as the absolute peak height is a more intuitive parameter than
+    the peak area.
+
+    References
+    ----------
+    .. [3] Kriesten et al. Chemometrics and Intelligent Laboratory Systems
+           91 (2008) 181-193.
+
+    """
+
+    alpha = parameter[0, :, None] # n_peaks x 1
+    beta = parameter[1, :, None] # n_peaks x 1
+    gamma2 = (parameter[2, :, None].T**2) # 1 x n_peaks
+    omega = parameter[3, :, None].T # 1 x n_peaks
+
+    # shift x coordinates
+    delta_x2 = (x-omega)**2 # n_wl x n_peaks
+
+    # calculate matrix of Gaussian peaks (n_wl x n_peaks)
+    gaussian_matrix = np.exp(-np.log(2) * delta_x2/gamma2)
+
+    # calculate matrix of Lorentzian peaks (n_wl x n_peaks)
+    lorentzian_matrix = 1/(1+delta_x2/gamma2)
+
+    # convert to vector by matrix multiplication w scaling factor
+    gaussian_vector = gaussian_matrix @ (alpha * beta)
+    lorentzian_vector = lorentzian_matrix @ (alpha * (1-beta))
+
+    return gaussian_vector + lorentzian_vector

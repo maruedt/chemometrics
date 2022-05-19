3e17fc81207175f8eab64ca63242fe14f92e353d
Matthias Rüdt <mruedt@icloud.com>
2022-05-19 21:12:05 +0200

    Transfer latent variable-related code into mixin
    - Add base.py for mixins
    - Move LV-related code from PLSRegression to LVmixin
    - Add LVmixin as parent to PLSRegression

--- chemometrics/regression.py
+++ chemometrics/regression.py
@@ -27,9 +27,10 @@ from scipy.optimize import least_squares
 from scipy.optimize._numdiff import approx_derivative
 import matplotlib.pyplot as plt
 from .utils import pseudo_voigt_spectra
+from .base import LVmixin
 
 
-class PLSRegression(_PLSRegression):
+class PLSRegression(_PLSRegression, LVmixin):
     r"""
     PLS regression with added chemometric functionality
 
@@ -52,6 +53,7 @@ class PLSRegression(_PLSRegression):
 
     def fit(self, X, Y):
         super().fit(X, Y)
+        self.n_samples_ = self.x_scores_.shape[0]
         self.vip_ = self._calculate_vip()
         self.x_residual_std_ = self._calculate_x_residual_std_(X)
         return self
@@ -79,20 +81,6 @@ class PLSRegression(_PLSRegression):
         counter = np.sum(ss * self.x_weights_**2, axis=1)
         denominator = np.sum(ss)
         return np.sqrt(self.n_features_in_ * counter / denominator)
-
-    def _calculate_x_residual_std_(self, X):
-        r"""
-        Calculate the standard deviation of the X residuals
-
-        The x residual standard deviation is calculated according to [1]_ not
-        including the correction factor v (since it is not exactly defined in
-        [1]_).
-        """
-        X_hat = self.inverse_transform(self.transform(X))
-        sse_cal = np.sum((X - X_hat)**2)
-        norm_factor = (self.x_scores_.shape[0] - self.n_components
-                       - 1) * (X.shape[1] - self.n_components)
-        return np.sqrt(sse_cal / norm_factor)
 
     def hat(self, X):
         r"""
@@ -191,90 +179,6 @@ class PLSRegression(_PLSRegression):
             raise(TypeError(f'unsupported scaling: {scaling}'))
 
         return residuals / scaling_factor
-
-    def dmodx(self, X, normalize=True, absolute=False):
-        r"""
-        Calculate distance to model hyperplane in X (DModX)
-
-        DModX provides the distance to the model hyperplane spanned by the
-        loading vectors. Any information in the predictors that is not captured
-        by the PLS model contributes to DModX. If the DModX is normalized,
-        DModX is devided by the mean residual variance of X observed during
-        model calibration.
-
-        Parameters
-        ----------
-        X : (n, m) ndarray
-            matrix of predictors. n samples x m predictors
-
-        normalize : {True (default); False}
-            normalization of DModX by error in X during calibration
-
-        absolute : {True; False (default)}
-            return the absolute distance to the model plane (not normalized by
-            degrees of freedom)
-
-        Returns
-        -------
-        dmodx : (n, ) ndarray
-            distance of n samples to model hyperplane
-
-        """
-
-        sse = np.sum((X - self.inverse_transform(self.transform(X)))**2,
-                     axis=1)
-        dmodx = np.sqrt(sse)
-
-        if not absolute:
-            dmodx /= np.sqrt(X.shape[1] - self.n_components)
-            if normalize:
-                dmodx /= self.x_residual_std_
-        return dmodx
-
-    def crit_dmodx(self, confidence=0.95):
-        r"""
-        Critical distance to hyperplane based on an F2 test
-
-        The critical distance to the model hyperplane is estimated based on
-        an F2 distribution. Values above crit_dmodx may be considered outliers.
-        dmodx is only approximately F2 distributed [1]_. It is thus worth
-        noting that the estimated critcal distance is biased. It however gives
-        a reasonable indication of points worth investigating.
-
-        """
-        degf_cali = self.n_features_in_ - self.n_components - 1
-        degf_test = self.n_features_in_ - self.n_components
-        f_crit = stats.f.ppf(confidence, degf_test,
-                             degf_cali)
-        return np.sqrt(f_crit)
-
-    def dhypx(self, X):
-        r"""
-        Normalized distance on hyperplane
-
-        Provides a distance on the hyperplane, normalized by the distance
-        observed during calibration. It can be a useful measure to see whether
-        new data is comparable to the calibration data. The normalized dhypx
-        is slightly biased towards larger values since the estimated
-        `x_residual_std_` is slightly underestimated during model calibration
-        [1]_.
-
-        """
-        var_cal = np.var(self.x_scores_, axis=0)
-        x_scores2_norm = self.transform(X)**2 / var_cal
-        return np.sum(x_scores2_norm, axis=1)
-
-    def crit_dhypx(self, confidence=0.95):
-        r"""
-        Calculate critical dhypx according to Hotelling's T2
-
-        """
-        comp = self.n_components
-        samples = self.x_scores_.shape[0]
-        f_crit = stats.f.ppf(confidence, self.n_components,
-                             self.x_scores_.shape[0]-self.n_components)
-        factor = comp * (samples**2 - 1) / (samples * (samples - comp))
-        return f_crit * factor
 
     def cooks_distance(self, X, Y):
         r"""
@@ -412,42 +316,6 @@ class PLSRegression(_PLSRegression):
         plt.ylabel('VIP')
 
         return fig.axes
-
-    def distance_plot(self, X, sample_id=None, confidence=0.95):
-        r"""
-        Plot distances colinear and orthogonal to model predictor hyperplane
-
-        Generates a figure with two subplots. The subplots provide information
-        on how `X` behaves compared to the calibration data. Subplots:
-        1) Distance in model hyperplane of predictors. Provides insight into
-        the magnitude of variation within the hyperplane compared to the
-        calibration data. Large values indicate samples which are outside of
-        the calibration space but may be described by linearly scaled latent
-        variables.
-        2) Distance orthogonal to model hyperplane. Provides insight into the
-        magnitude of variation orthogonal to the model hyperplane compared to
-        the calibration data. Large values indicate samples which show a
-        significant trend not observed in the calibration data.
-
-        """
-        plt.figure(figsize=(15, 15))
-
-        if not sample_id:
-            sample_id = np.arange(X.shape[0])
-        # make plots
-        # 1) dhypx
-        plt.subplot(211)
-        plt.plot(sample_id, self.dhypx(X))
-        plt.ylabel('X distance on hyperplane')
-        plt.axhline(y=self.crit_dhypx(confidence=confidence))
-
-        plt.subplot(212)
-        plt.plot(sample_id, self.dmodx(X))
-        plt.axhline(y=self.crit_dmodx(confidence=confidence))
-        plt.xlabel('Sample ID')
-        plt.ylabel('Distance to X-hyperplane')
-
-        return plt.gcf().axes
 
 
 def fit_pls(X, Y, pipeline=None, cv_object=None, max_lv=10):

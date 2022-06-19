61ec7d8f939f154a5028b77942456a3b51f3b8b2
Matthias Rüdt <mruedt@icloud.com>
2022-06-19 15:09:47 +0200

    Bump version number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, 2022 Matthias Rüdt"
 author = "Matthias Rüdt"
 
 # The full version, including alpha/beta/rc tags
-release = "0.3.0"
+release = "0.4.0"
 
 
 # -- General configuration ---------------------------------------------------
